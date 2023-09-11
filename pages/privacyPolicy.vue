3a26066e54428db01e89572902f7d87ef4717881
junosg <shards552@gmail.com>
2023-09-11 22:40:21 +0800

    replaced privacy policy page title

--- pages/privacyPolicy.vue
+++ pages/privacyPolicy.vue
@@ -32,7 +32,7 @@
 </template>
 <script setup lang="ts">
     definePageMeta({
-        title: 'About',
+        title: 'Privacy Policy',
         description: 'A no-cost statistical tool that autonomously determines the fitting test according to the supplied data. Its objective is to assist researchers in swiftly obtaining results for uncomplicated statistical analyses, and it is accessible even to those without prior knowledge of statistics.'
     });
 
