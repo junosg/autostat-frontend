58d189983948d37a79057439450dda64c5a3c538
junosg <shards552@gmail.com>
2023-10-19 00:06:54 +0800

    fixed logo display

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
     <section class="flex flex-col items-center justify-end py-40 w-full">
+        <section class="h-24 p-5 m-5 flex justify-start items-center">
+            <div class="text-6xl text-red-400 font-mono">autostat</div>
+        </section>
         <div class="text-gray-500 font-mono text-justify" :class="textSizeClass">
             A no-cost statistical tool that autonomously determines the fitting test according to the supplied data. Its objective is to assist researchers in swiftly obtaining results for uncomplicated statistical analyses, and it is accessible even to those without prior knowledge of statistics.
         </div>
